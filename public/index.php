6403f69a76159c1d533e9b15dbfca8323a4c593d
zepopa <kitik955@gmail.com>
2020-08-01 14:42:43 +0300

    question with execute

--- public/index.php
+++ public/index.php
@@ -3,17 +3,15 @@
 
 require "../vendor/autoload.php";
 
-$dbh = new \PDO('mysql:host=mysql;dbname=sandbox;','root','secret');
+$dbh = new \PDO('mysql:host=mysql;dbname=sandbox','root','secret');
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 function getPdo()
 {
     global $dbh;
     return $dbh;
 }
-abstract class Table
+abstract class Model
 {
-    protected int $id;
-
     public static function find(int $id)
     {
         $table = strtolower(static::class);
@@ -46,32 +44,45 @@ abstract class Table
     }
     public static function save(int $id = NULL,string $name,string $last_name,string $created_at,string $updated_at = NULL)
     {
+        $table = new static();
+        $temp = get_object_vars($table);
+        $fields = array();
+        $i = 0;
+        foreach ($temp as $key => $item){
+            $fields[$i] = $key;
+            $i++;
+        }
         if($id != NULL){
             $table = strtolower(static::class);
-            $sql = 'UPDATE '. $table .
-                ' SET name = '.'\''.$name.'\''.
-                ' , last_name = '.'\''.$last_name.'\''.
-                ' , created_at = '.'\''.$created_at.'\''.
-                ' , updated_at = '.'\''.$updated_at.'\''.
-                ' WHERE id = :id';
+            $sql = 'UPDATE '. $table . ' SET';
+            $len= count($fields);
+            for($i = 0;$i < $len;$i++){
+                if($i == 0) {
+                    $sql .=  ' '.$fields[$i].' = :' . $fields[$i];
+                }else{
+                    $sql .=  ', '.$fields[$i].' = :' . $fields[$i];
+                }
+            }
+            $sql .=' WHERE id = :id';
+//            var_dump($sql);
             $pdo = getPdo();
-            var_dump($sql);
             $stmt = $pdo->prepare($sql);
-            $stmt->execute([':id' => $id]);
+            $stmt->execute([':id' => $id, ':name'=>$name,':last_name'=>$last_name,':created_at'=>$created_at,':updated_at'=>$updated_at]);
         }
         else {
+//            array_shift($fields);
             $table = strtolower(static::class);
-            $sql = 'INSERT INTO '. $table .' (name,last_name,created_at,updated_at) VALUES ('.'\''.$name.'\''.','.'\''.$last_name.'\''.','.'\''.$created_at.'\''.','.'\''.$updated_at.'\''.')';
+            $sql = 'INSERT INTO '. $table .' ('.implode(", ",$fields).') VALUES (:'.implode(', :',$fields).')';
             $pdo = getPdo();
             var_dump($sql);
             $stmt = $pdo->prepare($sql);
-            $stmt->execute();
+            $stmt->execute([':id'=>$id ,':name'=>$name,':last_name'=>$last_name,':created_at'=>$created_at,':updated_at'=>$updated_at]);
         }
     }
 }
-class User extends Table
+class User extends Model
 {
-    protected int $id;
+    protected $id;
     protected $name;
     protected $last_name;
     protected $created_at;
