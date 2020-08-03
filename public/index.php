a7dff7cd2c0b342ac1c35d4db1647a46b093b178
DanyaKitik <kitik955@gmail.com>
2020-08-03 20:56:34 +0300

    commit

--- public/index.php
+++ public/index.php
@@ -3,84 +3,14 @@
 
 require "../vendor/autoload.php";
 
-$dbh = new \PDO('mysql:host=mysql;dbname=sandbox','root','secret');
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$dbh = new \PDO('mysql:host=mysql;dbname=test','root','secret');
+$dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 function getPdo()
 {
     global $dbh;
     return $dbh;
 }
-abstract class Model
-{
-    public static function find(int $id)
-    {
-        $table = strtolower(static::class);
-        $sql = 'SELECT * FROM '. $table .' WHERE id = :id';
-        $pdo = getPdo();
-        var_dump($sql);
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([':id' => $id]);
-        $userData = $stmt->fetch(\PDO::FETCH_ASSOC);
-
-        if($userData){
-            $user = new static();
-            foreach ($userData as $name => $value){
-                $user->$name = $value;
-            }
-            var_dump($user);
-        }
-        else{
-            throw new \Exception('there is nothing at this id');
-        }
-    }
-    public static function delete(int $id)
-    {
-        $table = strtolower(static::class);
-        $sql = 'DELETE FROM '. $table .' WHERE id = :id';
-        $pdo = getPdo();
-        var_dump($sql);
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([':id' => $id]);
-    }
-    public static function save(int $id = NULL,string $name,string $last_name,string $created_at,string $updated_at = NULL)
-    {
-        $table = new static();
-        $temp = get_object_vars($table);
-        $fields = array();
-        $i = 0;
-        foreach ($temp as $key => $item){
-            $fields[$i] = $key;
-            $i++;
-        }
-        if($id != NULL){
-            $table = strtolower(static::class);
-            $sql = 'UPDATE '. $table . ' SET';
-            $len= count($fields);
-            for($i = 0;$i < $len;$i++){
-                if($i == 0) {
-                    $sql .=  ' '.$fields[$i].' = :' . $fields[$i];
-                }else{
-                    $sql .=  ', '.$fields[$i].' = :' . $fields[$i];
-                }
-            }
-            $sql .=' WHERE id = :id';
-//            var_dump($sql);
-            $pdo = getPdo();
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([':id' => $id, ':name'=>$name,':last_name'=>$last_name,':created_at'=>$created_at,':updated_at'=>$updated_at]);
-        }
-        else {
-//            array_shift($fields);
-            $table = strtolower(static::class);
-            $sql = 'INSERT INTO '. $table .' ('.implode(", ",$fields).') VALUES (:'.implode(', :',$fields).')';
-            $pdo = getPdo();
-            var_dump($sql);
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([':id'=>$id ,':name'=>$name,':last_name'=>$last_name,':created_at'=>$created_at,':updated_at'=>$updated_at]);
-        }
-    }
-}
-class User extends Model
+class User extends \App\Model
 {
     protected $id;
     protected $name;
